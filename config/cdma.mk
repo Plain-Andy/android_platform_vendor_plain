ab64c1c0462c4a3343cf483a7ff0fffb04ff090c
Snuzzo <jrracinfan@gmail.com>
2014-02-17 20:01:02 -0500

    Initial commit

---
+++ config/cdma.mk
@@ -0,0 +1,3 @@
+# CDMA APN list
+PRODUCT_COPY_FILES += \
+    vendor/plain/prebuilt/etc/apns-conf-cdma.xml:system/etc/apns-conf.xml
---
+++ config/common.mk
@@ -0,0 +1,67 @@
+PRODUCT_BRAND ?= plain
+
+# general properties
+PRODUCT_PROPERTIES_OVERRIDE += \
+	keyguard.no_require_sim=true \
+	ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+	ro.com.google.clientidbase=android-google \
+	ro.com.android.wifi-watchlist=GoogleGuest \
+	ro.setupwizard.enterprise_mode=1 \
+	ro.com.android.dateformat=MM-dd-yyyy \
+	ro.com.android.dataroaming=false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.root_access=1
+
+# enable ADB authentication if not on eng build
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+endif
+
+# Embed SuperUser
+SUPERUSER_EMBEDDED := true
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/plain/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/plain/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/plain/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
+    vendor/plain/prebuilt/bin/blacklist:system/addon.d/blacklist
+
+# init.d support
+PRODUCT_COPY_FILES += \
+	vendor/plain/prebuilt/etc/init.d/00banner:system/etc/init.d/00banner \
+	vendor/plain/prebuilt/bin/sysinit:system/bin/sysinit
+
+# userinit support
+PRODUCT_COPY_FILES += \
+    vendor/plain/prebuilt/etc/init.d/90userinit:system/etc/init.d/90userinit
+
+# Init script file with plain extras
+PRODUCT_COPY_FILES += \
+    vendor/plain/prebuilt/etc/init.local.rc:root/init.plain.rc
+
+# Enable SIP and VoIP on all targets
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Additional packages
+-include vendor/plain/config/packages.mk
+
+# Versioning
+-include vendor/plain/config/version.mk
+
+# Add our overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/plain/overlay/common
+
+# Qualcomm Optimized Dalvik
+LOCAL_PATH:= vendor/plain/prebuilt
+
+ifneq ($(filter msm8610 msm8x26 msm8226 msm8x74 msm8974 msm8960 msm8660 msm7627a msm7630_surf apq8084 mpq8092,$(TARGET_BOARD_PLATFORM)),)
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/bin/dalvikvm:system/bin/dalvikvm \
+        $(LOCAL_PATH)/bin/dexopt:system/bin/dexopt \
+        $(LOCAL_PATH)/lib/libcutils.so:system/lib/libcutils.so \
+        $(LOCAL_PATH)/lib/libdvm.so:system/lib/libdvm.so \
+        $(LOCAL_PATH)/lib/libqc-opt.so:system/lib/libqc-opt.so
+endif
