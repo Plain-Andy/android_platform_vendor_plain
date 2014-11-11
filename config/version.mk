278d78616457262531906811b66bd21f536ebf0a
Snuzzo <jrracinfan@gmail.com>
2014-11-11 15:16:10 -0500

    Updated versioning

--- config/version.mk
+++ config/version.mk
@@ -5,28 +5,27 @@ TARGET_PRODUCT_SHORT := $(subst plain_,,$(TARGET_PRODUCT_SHORT))
 
 ifdef BUILDTYPE_NIGHTLY
 	ROM_BUILDTYPE := Nightly
-	ROM_VERSION := $(shell date -u +%Y%m%d)-$(TARGET_PRODUCT_SHORT)-Nightly
+endif
+ifdef BUILDTYPE_WEEKLY
+	ROM_BUILDTYPE := Weekly
 endif
 ifdef BUILDTYPE_EXPERIMENTAL
 	ROM_BUILDTYPE := Experimental
-	ROM_VERSION := $(shell date -u +%Y%m%d)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
-endif
-ifdef BUILDTYPE_RELEASE
-	ROM_BUILDTYPE := Release 
-	ROM_VERSION := $(PLATFORM_VERSION)-$(TARGET_PRODUCT_SHORT)
 endif
 
 ifndef ROM_BUILDTYPE
 	ROM_BUILDTYPE := Unofficial
-	ifneq ($(AROMA_BUILD),)
-	ROM_BUILDTYPE := $(ROM_BUILDTYPE)-Aroma
-	endif
-	ROM_VERSION := $(shell date -u +%Y%m%d)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
 endif
 ifeq ($(ROM_BUILDTYPE),Release)
 	$(shell export AROMA_BUILD=true)
 endif
 
+ROM_VERSION := $(shell date -u +%Y%m%d)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
+
+ifdef BUILDTYPE_RELEASE
+	ROM_BUILDTYPE := Release 
+	ROM_VERSION := $(PLATFORM_VERSION)-$(TARGET_PRODUCT_SHORT)
+endif
 
 # Apply it to build.prop
 PRODUCT_PROPERTY_OVERRIDES += \
