da6748d573aa5f56efbb2a73a69c5768b711d369
Snuzzo <jrracinfan@gmail.com>
2014-11-30 12:56:45 -0500

    (1/2) Use properties instead of variables for plain-tweak values

--- prebuilt/etc/init.plain.kerneltweak.sh
+++ prebuilt/etc/init.plain.kerneltweak.sh
@@ -4,24 +4,24 @@ then
 cp /sdcard/plaintweak /data/local/tmp/plaintweak
 chmod 0775 /data/local/tmp/plaintweak
 . /data/local/tmp/plaintweak
-echo $tcpcong > /proc/sys/net/ipv4/tcp_congestion_control
-echo $scheduler > /sys/block/mmcblk0/queue/scheduler
-echo $gov > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo $minkhz > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-echo $maxkhz > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+getprop tcpcong > /proc/sys/net/ipv4/tcp_congestion_control
+getprop scheduler > /sys/block/mmcblk0/queue/scheduler
+getprop gov > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+getprop minkhz > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+getprop maxkhz > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 if [ -d "/sys/devices/system/cpu/cpu1/" ]; then
-echo $gov > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-echo $minkhz > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-echo $maxkhz > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+getprop gov > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+getprop minkhz > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+getprop maxkhz > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
 fi
 if [ -d "/sys/devices/system/cpu/cpu2/" ]; then
-echo $gov > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-echo $minkhz > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
-echo $maxkhz > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+getprop gov > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+getprop minkhz > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+getprop maxkhz > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
 fi
 if [ -d "/sys/devices/system/cpu/cpu3/" ]; then
-echo $gov > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-echo $minkhz > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
-echo $maxkhz > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+getprop gov > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+getprop minkhz > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
+getprop maxkhz > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
 fi
 fi
