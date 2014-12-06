b3d7bae49a639b95cb43eea0c25c4f6b7aa83199
Snuzzo <jrracinfan@gmail.com>
2014-12-06 15:11:57 -0500

    Plain-Tweak v2.0; Add a watcher to detect changes Add ability to set cores 3&4 max/min cpu clockrates and governor Add logging

--- prebuilt/etc/init.plain.kerneltweak.sh
+++ prebuilt/etc/init.plain.kerneltweak.sh
@@ -4,10 +4,9 @@ then
 cp /sdcard/plaintweak /data/local/tmp/plaintweak
 chmod 0775 /data/local/tmp/plaintweak
 . /data/local/tmp/plaintweak
-
 if [ -z $tcpcong ]
 then
-setprop tcpcong $(cat /proc/sys/net/ipv4/tcp_congestion_control)
+setprop tcpcong $(getprop stocktcpcong)
 else
 setprop tcpcong $tcpcong
 echo $tcpcong > /proc/sys/net/ipv4/tcp_congestion_control
@@ -15,72 +14,109 @@ fi
 
 if [ -z $scheduler ]
 then
-setprop scheduler $(cat /sys/block/mmcblk0/queue/scheduler | sed 's/.*[[]//g' | sed 's/[]].*//g')
+setprop scheduler $(getprop stockscheduler)
 else
 setprop scheduler $scheduler
 echo $scheduler > /sys/block/mmcblk0/queue/scheduler
 fi
 
+if [ ! -z $gov2 ]
+then
+setprop gov2 $gov2
+else
+setprop gov2 $(getprop stockgov)
+fi
+
 if [ -z $gov ]
 then
-setprop gov $(ls /sys/devices/system/cpu/cpufreq)
+setprop gov $(getprop stockgov)
 else
 setprop gov $gov
 echo $gov > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-if [ -d "/sys/devices/system/cpu/cpu1/" ]; then
-echo $gov > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-fi
-if [ -d "/sys/devices/system/cpu/cpu2/" ]; then
-echo $gov > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-fi
-if [ -d "/sys/devices/system/cpu/cpu3/" ]; then
-echo $gov > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-fi
 fi
 
 if [ -z $minkhz ]
 then
-setprop minkhz $(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq)
+setprop minkhz $(getprop stockminkhz)
 else
 setprop minkhz $minkhz
 echo $minkhz > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-if [ -d "/sys/devices/system/cpu/cpu1/" ]; then
-echo $minkhz > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-fi
-if [ -d "/sys/devices/system/cpu/cpu2/" ]; then
-echo $minkhz > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
-fi
-if [ -d "/sys/devices/system/cpu/cpu3/" ]; then
-echo $minkhz > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
-fi
 fi
 
 if [ -z $maxkhz ]
 then
-setprop maxkhz $(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq)
+setprop maxkhz $(getprop stockmaxkhz)
 else
 setprop maxkhz $maxkhz
 echo $maxkhz > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-if [ -d "/sys/devices/system/cpu/cpu1/" ]; then
-echo $maxkhz > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
 fi
-if [ -d "/sys/devices/system/cpu/cpu2/" ]; then
-echo $maxkhz > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+
+if [ ! -z $maxkhz2 ]
+then
+setprop maxkhz2 $maxkhz2
+else
+setprop maxkhz2 $(getprop stockmaxkhz)
 fi
-if [ -d "/sys/devices/system/cpu/cpu3/" ]; then
-echo $maxkhz > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+
+if [ ! -z $minkhz2 ]
+then
+setprop minkhz2 $minkhz2
+else
+setprop minkhz2 $(getprop stockminkhz)
 fi
+
+if [ ! -z $updateinterval ]
+then
+setprop updateinterval $updateinterval
 fi
 
 else
-setprop tcpcong $(cat /proc/sys/net/ipv4/tcp_congestion_control)
-setprop scheduler $(cat /sys/block/mmcblk0/queue/scheduler | sed 's/.*[[]//g' | sed 's/[]].*//g')
-setprop gov $(ls /sys/devices/system/cpu/cpufreq)
-setprop minkhz $(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq)
-setprop maxkhz $(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq)
+setprop tcpcong $(getprop stocktcpcong)
+setprop scheduler $(getprop stockscheduler)
+setprop gov $(getprop stockgov)
+setprop minkhz $(getprop stockminkhz)
+setprop maxkhz $(getprop stockmaxkhz)
+if [ ! -z $maxkhz2 ]
+then
+setprop maxkhz2 $maxkhz2
+else
+setprop maxkhz2 $(getprop stockmaxkhz)
 fi
-echo "TCP Cong is"$(getprop tcpcong) > /data/local/tmp/plaintweak.log 
-echo "Scheduler is"$(getprop scheduler) >> /data/local/tmp/plaintweak.log
-echo "Governor is"$(getprop gov) >> /data/local/tmp/plaintweak.log
-echo "Minkhz is"$(getprop minkhz) >> /data/local/tmp/plaintweak.log
-echo "Maxkhz is"$(getprop maxkhz) >> /data/local/tmp/plaintweak.log
+
+if [ ! -z $minkhz2 ]
+then
+setprop minkhz2 $minkhz2
+else
+setprop minkhz2 $(getprop stockminkhz)
+fi
+if [ ! -z $gov2 ]
+then
+setprop gov2 $gov2
+else
+setprop gov2 $(getprop stockgov)
+fi
+echo $(getprop stocktcpcong) > /proc/sys/net/ipv4/tcp_congestion_control
+echo $(getprop stockscheduler) > /sys/block/mmcblk0/queue/scheduler
+echo $(getprop stockgov) > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo $(getprop stockminkhz) > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo $(getprop stockmaxkhz) > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+fi
+echo -n '\03 Plain-Tweak\0 LCD Density is '$(getprop customdensity)'\0' > /dev/log/main
+echo -n '\03 Plain-Tweak\0 Update interval is '$(getprop updateinterval)'\0' > /dev/log/main
+echo -n '\03 Plain-Tweak\0 TCP Cong is '$(getprop tcpcong)'\0' > /dev/log/main
+echo -n '\03 Plain-Tweak\0 Scheduler is '$(getprop scheduler)'\0' > /dev/log/main
+echo -n '\03 Plain-Tweak\0 Governor is '$(getprop gov)'\0' > /dev/log/main
+echo -n '\03 Plain-Tweak\0 Minkhz is '$(getprop minkhz)'\0' > /dev/log/main
+echo -n '\03 Plain-Tweak\0 Maxkhz is '$(getprop maxkhz)'\0' > /dev/log/main
+if [ ! -z $(getprop minkhz2) ]
+then
+echo -n '\03 Plain-Tweak\0 Minkhz cores 3/4 is '$(getprop minkhz2)'\0' > /dev/log/main
+fi
+if [ ! -z $(getprop maxkhz2) ]
+then
+echo -n '\03 Plain-Tweak\0 Maxkhz cores 3/4 is '$(getprop maxkhz2)'\0' > /dev/log/main
+fi
+if [ ! -z $(getprop gov2) ]
+then
+echo -n '\03 Plain-Tweak\0 Governor2 for cores 3/4 is '$(getprop gov2)'\0' > /dev/log/main
+fi
