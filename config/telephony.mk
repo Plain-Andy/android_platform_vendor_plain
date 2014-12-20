b2deaa23c9e190142ec9999ab3c86249ce4755f4
Snuzzo <jrracinfan@gmail.com>
2014-12-20 14:38:55 -0500

    Bring-up SPN's from @AICP

--- config/telephony.mk
+++ config/telephony.mk
@@ -1,5 +1,7 @@
 PRODUCT_COPY_FILES += \
-    vendor/plain/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/plain/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml \
+    vendor/plain/prebuilt/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml \
+    vendor/plain/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml
 
 PRODUCT_PACKAGES += \
     Stk \
