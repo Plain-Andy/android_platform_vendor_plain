0ad7e9414e3e53ce46d4b1aebd51b2ac6b5073de
Snuzzo <jrracinfan@gmail.com>
2014-12-20 14:17:50 -0500

    Telephony; clean-up

--- config/telephony.mk
+++ config/telephony.mk
@@ -1,7 +1,8 @@
-# GSM APN list
 PRODUCT_COPY_FILES += \
     vendor/plain/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
 
-# SIM Toolkit
 PRODUCT_PACKAGES += \
-    Stk
+    Stk \
+    CellBroadcastReceiver \
+    VoiceDialer \
+    SoundRecorder
