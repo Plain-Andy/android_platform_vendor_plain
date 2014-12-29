a41993265ae53e3c7c46ab2d7c202051f63ed8fa
Snuzzo <jrracinfan@gmail.com>
2014-12-29 14:51:27 -0500

    lunch proper devices from plain-build-targets

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/Plain-Andy/build-targets/lp/plain-build-targets | sed -e 's/#.*$//' | awk {'print $1'})
+for combo in $(curl -s https://raw.githubusercontent.com/Plain-Andy/build-targets/lp-legacy/plain-build-targets | sed -e 's/#.*$//' | awk {'print $1'})
 do
  add_lunch_combo plain_$(echo $combo)-user
 done
