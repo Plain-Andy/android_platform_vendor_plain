9216696ce9bfbe0e8c8b05f2a07ef3118101c8e6
Snuzzo <jrracinfan@gmail.com>
2015-01-09 02:55:16 -0500

    Switch to userdebug in lunch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 for combo in $(curl -s https://raw.githubusercontent.com/Plain-Andy-legacy/build-targets/lp-legacy/plain-build-targets | sed -e 's/#.*$//' | awk {'print $1'})
 do
- add_lunch_combo plain_$(echo $combo)-user
+ add_lunch_combo plain_$(echo $combo)-userdebug
 done
