588e563e3fc266b4a5c96a7d91a33ffd2aefb088
Snuzzo <jrracinfan@gmail.com>
2015-01-15 13:38:28 -0500

    Add boot animation toggle binary

--- config/common.mk
+++ config/common.mk
@@ -46,6 +46,7 @@ PRODUCT_COPY_FILES += \
     vendor/plain/prebuilt/etc/init.local.rc:root/init.aicp.rc \
     vendor/plain/prebuilt/etc/init.plain.kerneltweak.sh:system/etc/init.plain.kerneltweak.sh \
     vendor/plain/prebuilt/bin/plaintweak:system/bin/plaintweak \
+    vendor/plain/prebuilt/bin/bootanitoggle:system/bin/bootanitoggle \
     vendor/plain/prebuilt/bin/buildproptweaks.sh:system/bin/buildproptweaks \
     vendor/plain/prebuilt/etc/init.d/99customanimation:/system/etc/init.d/99customanimation \
     vendor/plain/prebuilt/etc/init.d/91customdensity:/system/etc/init.d/91customdensity \
