b4871d772efc873b717da349b5f1e8cefeef9e13
Snuzzo <jrracinfan@gmail.com>
2015-05-24 12:42:13 -0400

    sepolicy; fall in line with cm11.0

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,3 +1,8 @@
+#
+# This policy configuration will be used by all products that
+# inherit from CM
+#
+
 BOARD_SEPOLICY_DIRS += \
     vendor/plain/sepolicy
 
@@ -8,8 +13,8 @@ BOARD_SEPOLICY_UNION += \
     genfs_contexts \
     seapp_contexts \
     installd.te \
-    system.te \
     netd.te \
+    system.te \
     ueventd.te \
     vold.te \
     mac_permissions.xml
