2e1ef240fe9aa87052e87a6d546155f837cba74d
Snuzzo <jrracinfan@gmail.com>
2015-06-09 06:28:20 -0400

    PlainTweak; Property set & Custom Density
    -Source from vendor/build.prop
    -Use qemu density instead of custom property

--- prebuilt/etc/init.plain.kerneltweak.sh
+++ prebuilt/etc/init.plain.kerneltweak.sh
@@ -1,92 +1,91 @@
 #!/system/bin/sh
 sysrw
 restoreconfig(){
-mkdir -p /vendor/overlay/
-cat /sdcard/plaintweak > /vendor/overlay/local.prop
+cat /sdcard/plaintweak > /vendor/build.prop
 }
 propcompare(){
-if [ ! -e /vendor/overlay/local.prop ]; then
+if [ ! -e /vendor/build.prop ]; then
 restoreconfig
 fi
 . /sdcard/plaintweak
 echo "IO Scheduler"
-sed -i '/scheduler/d' /vendor/overlay/local.prop
+sed -i '/scheduler/d' /vendor/build.prop
 if [ ! -z $scheduler ]; then
 setprop scheduler $scheduler
 else
 setprop scheduler $(getprop stockscheduler)
 fi
-echo 'scheduler='$(getprop scheduler) >> /vendor/overlay/local.prop
+echo 'scheduler='$(getprop scheduler) >> /vendor/build.prop
 echo "set for "$(getprop scheduler)
 
 echo "TCP Congestion control"
-sed -i '/tcpcong/d' /vendor/overlay/local.prop
+sed -i '/tcpcong/d' /vendor/build.prop
 if [ ! -z $tcpcong ]; then
 setprop tcpcong $tcpcong
 else
 setprop tcpcong $(getprop stocktcpcong)
 fi
-echo 'tcpcong='$(getprop tcpcong) >> /vendor/overlay/local.prop
+echo 'tcpcong='$(getprop tcpcong) >> /vendor/build.prop
 echo "set for "$(getprop tcpcong)
 
 echo "Minkhz 1st set"
-sed -i '/minkhz/d' /vendor/overlay/local.prop
+sed -i '/minkhz/d' /vendor/build.prop
 if [ ! -z $minkhz ]; then
 setprop minkhz $minkhz
 else
 setprop minkhz $(getprop stockminkhz)
 fi
-echo 'minkhz='$(getprop minkhz) >> /vendor/overlay/local.prop
+echo 'minkhz='$(getprop minkhz) >> /vendor/build.prop
 echo "set for "$(getprop minkhz)
 
 echo "Minkhz 2nd set"
-sed -i '/minkhz2/d' /vendor/overlay/local.prop
+sed -i '/minkhz2/d' /vendor/build.prop
 if [ ! -z $minkhz2 ]; then
 setprop minkhz2 $minkhz2
 else
 setprop minkhz2 $(getprop stockminkhz)
 fi
-echo 'minkhz2='$(getprop minkhz2) >> /vendor/overlay/local.prop
+echo 'minkhz2='$(getprop minkhz2) >> /vendor/build.prop
 echo "set for "$(getprop minkhz2)
 
 echo "Gov 1st set"
-sed -i '/gov/d' /vendor/overlay/local.prop
+sed -i '/gov/d' /vendor/build.prop
 if [ ! -z $gov ]; then
 setprop gov $gov
 else
 setprop gov $(getprop stockgov)
 fi
-echo 'gov='$(getprop gov) >> /vendor/overlay/local.prop
+echo 'gov='$(getprop gov) >> /vendor/build.prop
 echo "set for "$(getprop gov)
 
 echo "Gov 2nd set"
-sed -i '/gov2/d' /vendor/overlay/local.prop
+sed -i '/gov2/d' /vendor/build.prop
 if [ ! -z $gov2 ]; then
 setprop gov2 $gov2
 else
 setprop gov2 $(getprop stockgov)
 fi
-echo 'gov2='$(getprop gov2) >> /vendor/overlay/local.prop
+echo 'gov2='$(getprop gov2) >> /vendor/build.prop
 echo "set for "$(getprop gov2)
 
 echo "Max Khz 1st set"
-sed -i '/maxkhz/d' /vendor/overlay/local.prop
+sed -i '/maxkhz/d' /vendor/build.prop
 if [ ! -z $maxkhz ]; then
 setprop maxkhz $maxkhz
 else
 setprop maxkhz $(getprop stockmaxkhz)
 fi
-echo 'maxkhz='$(getprop maxkhz) >> /vendor/overlay/local.prop
+echo 'maxkhz='$(getprop maxkhz) >> /vendor/build.prop
 echo "set for "$(getprop maxkhz)
 
 echo "Max Khz 2nd set"
-sed -i '/maxkhz2/d' /vendor/overlay/local.prop
+sed -i '/maxkhz2/d' /vendor/build.prop
 if [ ! -z $maxkhz2 ]; then
 setprop maxkhz2 $maxkhz2
 else
 setprop maxkhz2 $(getprop stockmaxkhz)
 fi
-echo 'maxkhz2='$(getprop maxkhz2) >> /vendor/overlay/local.prop
+echo 'maxkhz2='$(getprop maxkhz2) >> /vendor/build.prop
 echo "set for "$(getprop maxkhz2)
 }
 main(){
